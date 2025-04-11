fae8a8d397f92d169d2e545624ca2384b249b36a
/ᐠ. ｡.ᐟ\ᵐᵉᵒʷˎˊ˗ <thanapon9114@gmail.com>
2025-04-12 01:12:15 +0700

    add : files

--- miniproject/ex00/main.py
+++ miniproject/ex00/main.py
@@ -1,12 +1,12 @@
 from checkmate import checkmate
+import sys
 def main():
-    board = """\
-Q...
-..K.
-.P..
-....\
-"""
-    checkmate(board)
+    files = sys.argv
+    for i in files :
+        if i.find(".chess") != -1 :
+            with open(i, 'r') as files :
+                board = files.read()
+            checkmate(board)
 
 if __name__ == "__main__":
     main()
