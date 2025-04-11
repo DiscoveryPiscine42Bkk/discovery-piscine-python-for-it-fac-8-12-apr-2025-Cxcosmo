a579730bc517a1f756eca0a64d741005c5c6956e
/ᐠ. ｡.ᐟ\ᵐᵉᵒʷˎˊ˗ <thanapon9114@gmail.com>
2025-04-12 02:10:58 +0700

    fix : error output

--- miniproject/ex01/main.py
+++ miniproject/ex01/main.py
@@ -2,11 +2,14 @@ from checkmate import checkmate
 import sys
 def main():
     files = sys.argv
-    for i in files :
-        if i.find(".chess") != -1 :
-            with open(i, 'r') as files :
-                board = files.read()
-            checkmate(board)
+    if len(files) == 1 :
+        print("Need some file.chess")
+    else :
+        for i in files :
+            if i.find(".chess") != -1 :
+                with open(i, 'r') as files_chess :
+                    board = files_chess.read()
+                checkmate(board)
 
-if __name__ == "__main__":
+if __name__ == "__main__" :
     main()
